cefc74e5d159479b13d28afac75ba085f23d07e6
Mrinal Sharma <sharma.mr@husky.neu.edu>
2020-02-12 18:44:28 -0500

    Assignment 4 bill attachment functionality

--- src/main/java/com/csye6225/neu/repository/FileRepository.java
+++ src/main/java/com/csye6225/neu/repository/FileRepository.java
@@ -1,18 +1,13 @@
 package com.csye6225.neu.repository;
 
 import com.csye6225.neu.dto.Bill;
-import com.csye6225.neu.dto.User;
+import com.csye6225.neu.dto.FileAttachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
-import java.util.List;
 import java.util.UUID;
 @Transactional
 @Repository
-public interface BillRepository extends JpaRepository<Bill, UUID> {
-
-    public List<Bill> findByOwnerId(UUID id);
-
-
+public interface FileRepository extends JpaRepository<FileAttachment, UUID> {
 }
---
+++ src/main/java/com/csye6225/neu/service/FileService.java
@@ -0,0 +1,19 @@
+package com.csye6225.neu.service;
+
+import com.csye6225.neu.dto.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+public interface FileService {
+
+    public User authenticateUser(String auth);
+
+    public ResponseEntity<?> uploadFile(String auth, String id, MultipartFile file) throws IOException, NoSuchAlgorithmException;
+
+    public ResponseEntity<?> getFileById(String auth, String billId, String fileId);
+
+    public ResponseEntity<?> deleteFileById(String auth, String billId, String fileId);
+}
