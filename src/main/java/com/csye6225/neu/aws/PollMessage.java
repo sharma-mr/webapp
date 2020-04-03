9099ada3ecf36151ddfd57f5fd7ca49406e55077
Mrinal Sharma <sharma.mr@husky.neu.edu>
2020-04-02 18:18:18 -0700

    Enabling polling

---
+++ src/main/java/com/csye6225/neu/aws/PollMessage.java
@@ -0,0 +1,26 @@
+package com.csye6225.neu.aws;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+@Profile("aws")
+public class PollMessage {
+
+    @Autowired(required = false)
+    private AmazonSQSClient amazonSQSClient;
+
+    private Logger logger = LoggerFactory.getLogger(PollMessage.class);
+
+    @Scheduled(fixedRate=1000)
+    public void pollQueue(){
+        amazonSQSClient.receiveMessageAndDelete();
+    }
+
+}
